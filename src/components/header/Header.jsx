282e31150a0b5a94deac731d6c92ae0fec718a3b
Olumide Jones <olumideyjones@yahoo.com>
2022-07-27 22:17:40 +0200

    bubbles added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect } from 'react'
-import './header.css'
-import CTA from './CTA'
-import HeaderSocial from './HeaderSocials'
-import Mine from '../../assets/mine.PNG'
-import Mouse from '../../assets/mouse.png'
-import LogoClock from '../../assets/Nav/logoClock.jsx'
+import React, { useEffect } from "react";
+import "./header.css";
+import CTA from "./CTA";
+import HeaderSocial from "./HeaderSocials";
+import Mine from "../../assets/mine.PNG";
+import Mouse from "../../assets/mouse.png";
+import LogoClock from "../../assets/Nav/logoClock.jsx";
 
-import  {RiSingleQuotesL} from 'react-icons/ri'
-import {RiSingleQuotesR} from 'react-icons/ri'
+import { RiSingleQuotesL } from "react-icons/ri";
+import { RiSingleQuotesR } from "react-icons/ri";
 //aos
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 /* import {BsMouse} from 'react-icons/bs' */
 
 /* import Spline  from "@splinetool/react-spline" */
 
 const Header = () => {
-
-   //aos 
-   useEffect(() => {
+  //aos
+  useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
- 
 
   return (
     <header id="home">
@@ -39,10 +37,30 @@ const Header = () => {
         <div className="typingContainer">
           <div className="text">
             <ul>
-              <li><span className="span1"><RiSingleQuotesL className="rIcon"/> Fullstack Developer <RiSingleQuotesR className="rIcon"/></span></li>
-              <li><span className="span2"><RiSingleQuotesL className="rIcon"/> Designer <RiSingleQuotesR className="rIcon"/></span></li>
-              <li><span className="span3"><RiSingleQuotesL className="rIcon"/> Musician <RiSingleQuotesR className="rIcon"/></span></li>
-              <li><span className="span4"><RiSingleQuotesL className="rIcon"/> Content creator <RiSingleQuotesR className="rIcon"/></span></li>
+              <li>
+                <span className="span1">
+                  <RiSingleQuotesL className="rIcon" /> Fullstack Developer{" "}
+                  <RiSingleQuotesR className="rIcon" />
+                </span>
+              </li>
+              <li>
+                <span className="span2">
+                  <RiSingleQuotesL className="rIcon" /> Designer{" "}
+                  <RiSingleQuotesR className="rIcon" />
+                </span>
+              </li>
+              <li>
+                <span className="span3">
+                  <RiSingleQuotesL className="rIcon" /> Musician{" "}
+                  <RiSingleQuotesR className="rIcon" />
+                </span>
+              </li>
+              <li>
+                <span className="span4">
+                  <RiSingleQuotesL className="rIcon" /> Content creator{" "}
+                  <RiSingleQuotesR className="rIcon" />
+                </span>
+              </li>
             </ul>
           </div>
         </div>
@@ -50,16 +68,32 @@ const Header = () => {
         <CTA />
         <HeaderSocial />
 
+        {/*me*/}
         <div className="me">
-           <img src={Mine} alt="me" data-aos="flip-right"/>
+          <img src={Mine} alt="me" data-aos="flip-right" />
         </div>
 
-        <a href="#contact" className='scroll__down'>Scroll Down </a>
-        <a href="#contact" className="scroll__down_anchor"><img className="mouse" src={Mouse} alt="mouse" /></a>
-      </div>
-      
-    </header>
-  )
-}
+        {/*bubbles*/}
+        <div className="bubbles">
+          <span className="language">html</span>
+          <span className="language">css</span>
+          <span className="language">javascript</span>
+          <span className="language">react.js</span>
+          <span className="language">node.js</span>
+          <span className="language">express.js</span>
+          <span className="language">mongodb</span>
+        </div>
 
-export default Header
+        {/*mouse*/}
+        <a href="#contact" className="scroll__down">
+          Scroll Down{" "}
+        </a>
+        <a href="#contact" className="scroll__down_anchor">
+          <img className="mouse" src={Mouse} alt="mouse" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
